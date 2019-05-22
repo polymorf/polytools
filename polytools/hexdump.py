6e271d3459f3e56e48da7f6b1e642981c55c6c4e
David BERARD <contact@davidberard.fr>
2019-05-22 20:58:17 +0200

    python3

--- polytools/hexdump.py
+++ polytools/hexdump.py
@@ -24,15 +24,15 @@ def hexdump(buf,title="",color=6,start=0):
 	if title != "":
 		dashlen = (46-len(title))/2
 		out=("           %s┌"+"─"*dashlen+"  "+title+"  "+"─"*(dashlen-(1-(len(title)%2)))+"┬"+"─"*18+"┐%s\n") % (color_start,color_stop)
-	for i in xrange(0,len(buf),16):
+	for i in range(0,len(buf),16):
 		out+="%s0x%08x │ %s" % (color_start,i+start,color_stop)
-		for j in xrange(16):
+		for j in range(16):
 			if i+j < len(buf):
 				out+="%02x " % (ord(buf[i+j]))
 			else:
 				out+="   "
 		out+="%s│ %s" % (color_start,color_stop)
-		for j in xrange(16):
+		for j in range(16):
 			if i+j < len(buf):
 				if buf[i+j] in string.printable and buf[i+j] not in "\t\n\r\x0b\x0c":
 					out+="%s" % (buf[i+j])
