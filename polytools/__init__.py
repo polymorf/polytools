87b155dad0e9563cd74118a9935c204f6a9574b3
David BERARD <contact@davidberard.fr>
2021-09-22 11:29:36 +0200

    remove old dir

--- polytools/__init__.py
+++
@@ -1,5 +0,0 @@
-from polytools.hexdump import hexdump, load_hexdump
-from polytools.log import *
-from polytools.print_obj import print_obj
-from polytools.crypto import *
-from polytools.fuzzer import *
