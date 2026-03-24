33458b9298c01b9030399598efb62d92128738f7
David BERARD <contact@davidberard.fr>
2026-03-24 15:33:29 +0100

    fix

--- python/polytools/__init__.py
+++ python/polytools/__init__.py
@@ -1,4 +1,4 @@
-from polytools.hexdump import hexdump, load_hexdump
+from polytools.hexdump import hexdump
 from polytools.log import *
 from polytools.print_obj import print_obj
 from polytools.crypto import *
